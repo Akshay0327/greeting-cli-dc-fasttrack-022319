a93ea4d1542629743093f0c9a35aa23c106ccb0e
Akshay Sharma <akshay.sharma09@gmail.com>
2019-06-09 15:26:35 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,4 +1,8 @@
 # code the #greeting method here!
 def greeting(name)
+<<<<<<< HEAD
   puts"Hello #{name.chomp.strip}. It's nice to meet you."
+=======
+  puts"Hello #{name}. It's nice to meet you."
+>>>>>>> 76e9eb1d2a0cf3fa5280821ae77fc64a5ee42abe
 end
